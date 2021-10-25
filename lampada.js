aa4f3763958d10811fa34221be0cb75820d284fd
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-10-25 13:29:26 -0300

    Lampada

---
+++ lampada.js
@@ -0,0 +1,29 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+const isLampBroken = () => {
+    return lamp.src.indexOf('quebrada') > -1;
+};
+
+const lampOn = () => {
+    if (!isLampBroken()) {
+        lamp.src = './images/ligada.jpg';
+    }
+};
+
+const lampOff = () => {
+    if (!isLampBroken()) {
+        lamp.src = './images/desligada.jpg';
+    }
+};
+
+const lampBroken = () => {
+    lamp.src = './images/quebrada.jpg';
+};
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
